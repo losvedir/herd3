a80446f49b09f0014242b12ecc601a4b95a91328
Gabe Durazo <gabe@gabedurazo.com>
2014-05-16 01:13:35 -0400

    [GJD] Serve static assets and hook up simple CSS and JS

--- Main.hs
+++ Main.hs
@@ -2,9 +2,12 @@
 import Web.Scotty
 import qualified Text.Blaze.Html.Renderer.Text as T
 import qualified Herd3.Views.Index
+import Network.Wai.Middleware.Static
 
 blaze = html . T.renderHtml
 
 main = scotty 3000 $ do
+  middleware $ staticPolicy (addBase "herd3/assets/")
+
   get "/" $ do
     blaze Herd3.Views.Index.render
--- herd3/views/index.hs
+++ herd3/views/index.hs
@@ -3,11 +3,22 @@
 module Herd3.Views.Index where
 
 import Text.Blaze.Html5
+import Text.Blaze.Html5.Attributes
+import qualified Text.Blaze.Html5 as H
 
 render = do
-  html $ do
+  docTypeHtml $ do
+    H.head $ do
+      H.title "Welcome to the Future. HERD3 Analytics Dashboard."
+      link ! rel "stylesheet" ! href "css/herd3.css"
     body $ do
-      h1 "My todo list"
-      ul $ do
-        li "learn haskell"
-        li "make a website"
+      h1 "Analytics Dashboard"
+      H.div $ do
+        p "Possible graphs:"
+        ul $ do
+          li "date histogram of how many coaches have been approved over time"
+          li "pie chart of coaches by sport"
+          li "count of messages over time"
+          li "sum of package revenue over time"
+      p "Also: Control panel to filter and select stuff."
+      script ! src "js/herd3.js" $ ""
